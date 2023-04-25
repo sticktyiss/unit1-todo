562ea92aca85df54713ea0ffd087b5710b5bc810
Tyiss Goodman <sticktyiss@gmail.com>
2023-04-25 15:26:46 -0700

    Changed App.js to implement component funcitonality

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-import ListItem from './ListItem';
+import logo from "./logo.svg";
+import "./App.css";
+import ListItem from "./ListItem";
+import { useState } from "react";
 
 function App() {
+  const [itemList, setItemList] = useState([
+    "Buy eggs",
+    "Finish cleaning room",
+    "Plan proposal",
+    "Play games",
+    "Work on websites"
+  ])
+
   return (
     <div className="App">
       this is app.jsx
-      <ListItem item='Buy eggs'/>
-      <ListItem item='Finish cleaning room'/>
-      <ListItem item='Plan proposal'/>
-      <ListItem item='Play games'/>
-      <ListItem item='Work on websites'/>
-    </div>
+      <ListItem item={itemList[0]} />
+      <ListItem item={itemList[1]} />
+      <ListItem item={itemList[2]} />
+      <ListItem item={itemList[3]} />
+      <ListItem item={itemList[4]} />
+      </div>
   );
 }
 
